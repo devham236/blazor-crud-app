5cd153782cfb05931657c8a7f9e0a61c467deb65
devham236 <90037750+devham236@users.noreply.github.com>
2024-01-10 09:29:58 +0100

    test

--- GameStore.Client/obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
+++ GameStore.Client/obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6785d4d7f2d83a62bf35dfb3f86d243302b053")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
